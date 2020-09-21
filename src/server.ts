041979f1e02ba580900ff369e418912f82e404d6
Paul Dlug <paul.dlug@gmail.com>
2020-09-21 12:53:42 -0400

    refactor: move schema to .graphql file

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,13 @@
 import dotenv from "dotenv";
 process.env.NODE_ENV !== "production" && dotenv.config();
 
-import { ApolloServer } from "apollo-server";
-
-import { typeDefs } from "./graphql/schema";
+import { ApolloServer, gql } from "apollo-server";
+import fs from "fs";
 import { resolvers } from "./graphql/resolvers";
+
+const typeDefs = gql`
+  ${fs.readFileSync(__dirname.concat("/graphql/schema.graphql"), "utf8")}
+`;
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
